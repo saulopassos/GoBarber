ea20b610a67cf67b04a9e0404c7d10df2f8590b6
Saulo Passos <saulopassos@MacBook-Pro-de-Saulo.local>
2019-10-30 15:14:25 -0300

    Ambiente e Conceitos

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  dialect: 'mysql',
+  dialect: 'postgres',
   host: 'localhost',
-  username: 'root',
-  password: '',
+  username: 'postgres',
+  password: 'docker',
   database: 'gobarber',
   define: {
     timestamps: true,
